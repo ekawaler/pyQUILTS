d6740789035858b1eb0f7ca03eab7d61c2187632
ekawaler <e.kawaler@gmail.com>
2017-01-06 17:18:35 -0500

    chunk size as a parameter
    Now it doesn’t always have to be a million! Defaults to that, though.

--- exonSearchTree.py
+++ exonSearchTree.py
@@ -4,38 +4,42 @@ Then again, it might slow it down. Maybe I'll test it both ways.
 Or maybe I won't. Who knows what I'll do? I'm a WILD CARD
 
 It's a several-layer tree. There's a chromosome node, which has daughter "chunk" nodes (one chunk for each million
-BP in a chromosome), which have leaf nodes for each exon. (An exon that spans two chunk nodes will appear
-in both, since we'll be searching for single positions...I think.)
+BP in a chromosome), which have leaf nodes for each exon. (An exon that spans multiple chunk nodes will appear
+in all, since we'll be searching for single positions...I think.)
 '''
 
 class ExonSearchTree:
-	def __init__(self):
+	def __init__(self, chunk_size = 1000000):
 		self.chr_nodes = {}
+		self.chunk_size = chunk_size
 	
 	def print_tree(self):
 		for node in self.chr_nodes:
 			self.chr_nodes[node].print_node()
 	
 	def add_exon(self, chr, start, end, name = "."):
+		chr = str(chr)
 		try:
 			self.chr_nodes[chr].add_node(start, end, name)
 		except KeyError:
-			self.chr_nodes[chr] = ESTChrNode(chr)
+			self.chr_nodes[chr] = ESTChrNode(chr, self.chunk_size)
 			self.chr_nodes[chr].add_node(start, end, name)
 			
 	def find_exon(self, chr, pos):
+		chr = str(chr)
 		return self.chr_nodes[chr].find_exon(pos)
 
 class ESTChrNode:
 	# A layer of chromosome nodes
-	def __init__(self, chr_num):
+	def __init__(self, chr_num, chunk_size):
 		self.chr_num = chr_num
 		self.chunk_nodes = {}
+		self.chunk_size = chunk_size
 	
 	def add_node(self, start, end, name):
-		chunks = {start/1000000}
-		chunks.add(end/1000000)
-		for chunk in chunks:
+		start_chunk = start/self.chunk_size
+		end_chunk = end/self.chunk_size
+		for chunk in range(start_chunk, end_chunk+1):
 			try:
 				self.chunk_nodes[chunk].add_node(start, end, name)
 			except KeyError:
@@ -43,7 +47,7 @@ class ESTChrNode:
 				self.chunk_nodes[chunk].add_node(start, end, name)
 	
 	def find_exon(self, pos):
-		chunk = pos/1000000
+		chunk = pos/self.chunk_size
 		return self.chunk_nodes[chunk].find_exon(pos)
 
 	def print_node(self):
@@ -85,9 +89,10 @@ class ESTExonLeaf:
 		print self.chr, self.start, self.end, self.name
 		
 if __name__=="__main__":
-	est = ExonSearchTree()
-	est.add_exon(1, 100000000, 100050000, "NP_5")
+	est = ExonSearchTree(1000)
+	est.add_exon(1, 100000000, 100002000, "NP_5")
 	est.add_exon('M', 999990, 1000500, "NP_6")
 	est.add_exon('Q', 3939, 8858)
 	est.print_tree()
 	print est.find_exon('M', 999995)
+	print est.find_exon('1', 100000001)
