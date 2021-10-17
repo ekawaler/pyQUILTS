99cc155ef3104043ac58f1a0dbd928dce28793d0
ekawaler <e.kawaler@gmail.com>
2021-10-17 18:56:18 -0400

    Update runtest.sh
    Different set of arguments for this version

--- runtest.sh
+++ runtest.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python quilts.py -—output_dir /samples/em_test —-genome /samples/em_test/genome —-proteome /samples/em_test/proteome —-germline /samples/em_test/germline --somatic /samples/em_test/somatic --junction /samples/em_test/junctions —-junction_file_type mapsplice —-threshB 1 --threshD 1 --threshN 1 --fusion /samples/em_test/fusions
+python quilts.py -—output_dir /samples/em_test —-genome /samples/em_test/genome —-proteome /samples/em_test/proteome —-germline /samples/em_test/germline --somatic /samples/em_test/somatic
